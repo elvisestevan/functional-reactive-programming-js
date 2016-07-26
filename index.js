2b35ec40363318a2e0b678060fcbe9406e7e826d
Elvis Estevan <elvis.estevan65@gmail.com>
2016-07-25 23:03:48 -0300

    MongoDB config file removed, it will be used pokemon api as a server of the app

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ var app = express();
 var http = require('http').Server(app);
 var path = require('path');
 
-var mongoose = require('mongoose');
-var configDB = require('./server/config/database.js');
-
-mongoose.connect(configDB.url);
-
-var bodyParser = require('body-parser');
-
-app.use(bodyParser.json());
-
 var port = process.env.PORT;
 
 app.use(express.static(path.resolve(__dirname, 'public')));
